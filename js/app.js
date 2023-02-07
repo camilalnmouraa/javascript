3d25626c315ec3f86aa73630bdb18e5251c45d16
Camila Moura <camilalnmoura@gmail.com>
2023-02-07 20:44:34 -0300

    Operadores de Comparação

--- js/app.js
+++ js/app.js
@@ -17,11 +17,41 @@ document.getElementById('user-Name').innerHTML = userName
 
 // Operadores Matemáticos //
 
-var n1 = 7
-var n2 = 2.5
+//var n1 = 7
+//var n2 = 2.5
 
-console.log(typeof n1)
-console.log(typeof n2)
+//console.log(typeof n1)
+//console.log(typeof n2)
 
-var total = n1 / n2
-console.log(total)
+//var total = n1 / n2
+//console.log(total)
+
+// Operadores de Comparação //
+
+var v1 = 5
+var v2 = 5
+
+//Igual (==)
+var resultado = v1 == v2
+console.log(resultado)
+
+//Diferente(!=)
+var resultado = v1 != v2
+console.log(resultado)
+
+//Igual valor e tipo (===)
+var resultado = v1 === v2
+console.log(resultado)
+
+//Maior que (>)
+var resultado = v1 > v2
+console.log(resultado)
+
+//Maior ou igual que (>=)
+var resultado = v1 >= v2
+console.log(resultado)
+
+//Menor ou igual que (<=)
+var resultado = v1 <= v2
+console.log(resultado)
+
