63ff653c84b10fad7cdead63490b82ed97d1a2d4
Camila Moura <camilalnmoura@gmail.com>
2023-02-07 20:23:00 -0300

    Variáveis de tipos de dados

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 console.log('Olá, Javascript!')
+
+var userName = 'Camila Moura'
+
+document.getElementById('user-Name').innerHTML = userName
+
+//var nome = 'Mestre Yoda'
+//var idade = 100
+//var jedi = true
+
+//console.log(typeof nome)
+//console.log(typeof idade)
+//console.log(typeof jedi)
+
